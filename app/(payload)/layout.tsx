cf33dca257a66ec4b2ada5093dade85570e084f1
Lucas Liew <lucasliew88@gmail.com>
2026-01-23 18:57:58 +0800

    fixed payload load error

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,9 +1,8 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 import configPromise from '@payload-config'
 import '@payloadcms/next/css'
-import { RootLayout } from '@payloadcms/next/layouts'
+import { RootLayout, handleServerFunctions } from '@payloadcms/next/layouts'
 /* eslint-disable-next-line import/no-unresolved */
-import { handleServerFunctions } from '@payloadcms/next/utilities'
 import React from 'react'
 
 import { importMap } from './admin/importMap'
